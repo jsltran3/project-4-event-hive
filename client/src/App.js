fdd01375036156720b175da54f87ba6cdbfcce24
Joseph <jsltran@gmail.com>
2023-01-24 14:38:20 -0800

    add forms for sign up

--- client/src/App.js
+++ client/src/App.js
@@ -4,29 +4,44 @@ import Login from './Login.js';
 import Home from './Home.js';
 import SignupForm from './SignupForm.js';
 import NavBar from './NavBar.js';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Routes  } from "react-router-dom";
+
+
 
 
 function App() {
+  
   return (
     <div className="App">
-      <p>wtf</p>
- 
       <NavBar />
-      <Switch>
-      <Route path="/">
-        <Home />
-      </Route>
-      <Route path="/signup">
-        <SignupForm />
-      </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-    </Switch>
-
+        <Routes>
+          <Route path="/Signup" element={ <SignupForm/> } />
+          <Route path="/" element={ <Home/> } />
+          <Route path="/Login" element={ <Login/> } />
+        </Routes>
     </div>
   );
 }
+  
+  
+//   return (
+//     <div className="App">
+//       <p>wtf</p>
+ 
+//       <NavBar />
+//       <Routes >
+//       <Route path="/">
+//         <Home />
+//       </Route>
+//       <Route path="/signup">
+//         <SignupForm />
+//       </Route>
+//       <Route path="/login">
+//         <Login />
+//       </Route>
+//       <Routes >
+//     </div>
+//   );
+// }
 
 export default App;
--- client/src/SignupForm.js
+++ client/src/SignupForm.js
@@ -10,23 +10,17 @@ function SignupForm() {
 	const [error, setError] = useState(false); 
 
 	// name change handler
-	const handleName = (e) => {
+	const handleUserName = (e) => {
 		setUserName(e.target.value);
 		setSubmitted(false);
 	}
 
-	//email change  handler
-	const handleEmail = (e) => {
+	// name change handler
+	const handlePassword = (e) => {
 		setUserName(e.target.value);
 		setSubmitted(false);
-		};
+	}
 
-const handlePassword = (e) => {
-	setPassword(e.target.value);
-	setSubmitted(false);
-	};
-
-	// Handling the form submission
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		if (userName === '' || password === '') {
@@ -37,56 +31,27 @@ const handlePassword = (e) => {
 		}
 		};
 
-	// Showing success message
-const successMessage = () => {
 	return (
-	<div
-		className="success"
-		style={{
-		display: submitted ? '' : 'none',
-		}}
-	>
-	<h1>User {userName} successfully registered!!</h1>
-	</div>
-	)
-	};
-	
-	// Showing error message if error is true
-	const errorMessage = () => {
-
-	return (
-		<div className="form">
 		<div>
-		<h1>User Registration</h1>
-		</div>
-		
-		{/* Calling to the methods */}
-		<div className="messages">
-		{errorMessage()}
-		{successMessage()}
-		</div>
-		
-		<form>
-		{/* Labels and inputs for form data */}
-		<label className="label">Name</label>
-		<input onChange={handleName} className="input"
-		value={userName} type="text" />
-		
-		{/* <label className="label">Email</label>
-		<input onChange={handleEmail} className="input"
-		value={email} type="email" /> */}
-		
-		<label className="label">Password</label>
-		<input onChange={handlePassword} className="input"
-		value={password} type="password" />
-		
-		<button onClick={handleSubmit} className="btn" type="submit">
-		Submit
-		</button>
-		</form>
-		</div>
-		);
-		}
+			<p>I am sign up!</p>
+			<form>
+{/* Labels and inputs for form data */}
+<label className="label">Name</label>
+<input onChange={handleUserName} className="input"
+value={userName} type="text" />
 
-	}
+<label className="label">Password</label>
+<input onChange={handlePassword} className="input"
+value={password} type="password" />
+
+<button onClick={handleSubmit} className="btn" type="submit">
+Submit
+</button>
+</form>
+		</div>
+	);
+}
+	
 export default SignupForm;
+
+
