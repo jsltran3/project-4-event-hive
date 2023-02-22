5146d1e6c8552558dcaaff32057c92bfaa50dfca
Joseph <jsltran@gmail.com>
2023-02-21 22:06:00 -0800

    controller skeleton

--- app/serializers/concert_ticket_serializer.rb
+++ app/serializers/concert_ticket_serializer.rb
@@ -1,3 +1,9 @@
 class ConcertTicketSerializer < ActiveModel::Serializer
   attributes :id, :title, :band_id, :user_id
 end
+
+attributes :id, :name, :start_time, :end_time
+
+has_many :foods
+has_many :users, through: :foods
+end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :user_name, :password_digest
 end
+
+
+attributes :id, :username
+
+has_many :foods
+has_many :cookouts, through: :foods
+end
