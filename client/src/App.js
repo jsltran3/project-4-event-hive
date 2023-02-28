b5be218ddf521c4bd29c651482cfeb282f7557bd
Joseph <jsltran@gmail.com>
2023-02-28 14:41:36 -0800

    pull auto-login into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Routes  } from "react-router-dom";
-import './App.css';
 import Login from './Login';
 import About from './Home.js';
 import SignupForm from './LoginForm.js';
 import NavBar from './NavBar.js';
+import ConcertTicket from "./components/ConcertTix/ConcertTickets";
+import Band from "./components/Band/Band";
 
 
 
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [concertTicket, setconcertTicket] = useState([]);
+  const [chosenConcert, setChosenConcert] = useState({});
+  const [concertTicketIndex, setConcertTicketIndex] = useState("");
+  const [bandOptions, setBandOptions] = useState([]);
+  const [bandId, setbandId] = useState("");
+  const [bandIndex, setBandIndex] = useState("");
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json()
+        .then((user) => {
+          setUser(user);
+        })
+    }
+  });
+  }, []);
   
   return (
     <div className="App">
--- client/src/components/ConcertTix/ConcertTickets.js
+++ client/src/components/ConcertTix/ConcertTickets.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import ConcertTixForm from "./AddCookoutForm";
+import ConcertTixForm from "./ConcertTixForm";
 import EditConcertTixForm from "./EditConcertTixForm"
 
 function ConcertTicket() {
