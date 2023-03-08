c2a1d532bcc8c6d35069426f60b300cdc47ab66f
Joseph <jsltran@gmail.com>
2023-03-07 20:24:28 -0800

    latest

--- app/serializers/concert_ticket_serializer.rb
+++ app/serializers/concert_ticket_serializer.rb
@@ -1,6 +1,6 @@
 class ConcertTicketSerializer < ActiveModel::Serializer
-  attributes :id, :title, :band_id, :user_id
-end
+#   attributes :id, :title, :band_id, :user_id
+# end
 
 attributes :id, :name, :start_time, :end_time
 
